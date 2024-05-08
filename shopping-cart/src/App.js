2b308c4044cf6e8720e18c824b99f46db5a97460
+ <+>
2024-05-08 15:50:44 +0300

    done

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,10 +1,12 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import MainContent from "./component/MainContent";
+import Navbar from "./component/Navbar";
 
 function App() {
   return <>
       <MainContent />
+      <Navbar/>
       
   </>;
 }
