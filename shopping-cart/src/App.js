e2c45583d7f9903e1adb4bca8169361608157200
martin-XPS-15-9510 <oowuoroowuor@gmail.com>
2024-05-08 22:22:11 +0300

    update commit

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,14 +1,23 @@
-// import logo from "./logo.svg";
-import "./App.css";
-import MainContent from "./component/MainContent";
-import Navbar from "./component/Navbar";
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import CustomersList from "./CustomersList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return <>
-      <MainContent />
-      <Navbar/>
-      
-  </>;
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/customers" element={<CustomersList />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
