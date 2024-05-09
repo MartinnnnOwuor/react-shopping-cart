455cb0b5141f8de39d595e486f6fec9d400771fe
Felix Mutuma <felicmutuma8@gmail.com>
2024-05-09 11:01:28 +0300

    comits about component

--- shopping-cart/src/component/About.jsx
+++ shopping-cart/src/component/About.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      
+    <div className="about-section">
+    <h2>About Us</h2>
+    <p>
+      Welcome to Ekart shopping centre! We strive to provide you with the best shopping experience possible.
+    </p>
+    <div className="contact-info">
+      <h3>Contact Information</h3>
+      <p>Email: Ekarte.ecommerce.com</p>
+      <p>Phone: 123-456-7890</p>
+      <p>Address: 123 Main Street, Kenya</p>
     </div>
+    <div className="social-media">
+      <h3>Follow Us</h3>
+      <ul>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Instagram</a></li>
+      </ul>
+    </div>
+    <div className="legal-info">
+      <h3>Legal Information</h3>
+      <p>Copyright © 2024 E-commerce Site. All rights reserved.</p>
+      <p>Terms of Service | Privacy Policy</p>
+    </div>
+  </div>
   )
 }
 
